006f3c41a1f3b804ca55c6450301d6496fa6c12f
Devin Elliot Gomez <devinelliotgomez@gmail.com>
2023-11-06 13:13:08 -0600

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Code your solution here
+const drivers = [
+  {
+    name: "Bobby",
+    hometown: "Pittsburgh",
+  },
+  {
+    name: "Sammy",
+    hometown: "New York",
+  },
+  {
+    name: "Sally",
+    hometown: "Cleveland",
+  },
+  {
+    name: "Annette",
+    hometown: "Los Angeles",
+  },
+  {
+    name: "Bobby",
+    hometown: "Tampa Bay",
+  },
+];
+
+function findMatching(array, name) {
+  const filteredArray = array.filter(function (driverName) {
+    return driverName.toLowerCase() === name.toLowerCase();
+  });
+  return filteredArray;
+}
+
+// function fuzzyMatch(array, firstLetter) {
+//   return array.filter((array) => array.startsWith(firstLetter));
+// }
+
+function fuzzyMatch(array, firstLetter) {
+  const filteredArray = array.filter(function (driverName) {
+    return driverName.startsWith(firstLetter);
+  });
+  return filteredArray;
+}
+
+function matchName(drivers, name) {
+  return drivers.filter((driver) => driver.name === name);
+}
